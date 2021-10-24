dc3b5a7f709f4271e7f05386c92675298470cb54
Jaime Alejandro Rios <37629779+jaiimeriios@users.noreply.github.com>
2021-10-24 15:32:28 -0700

    :leaves:  DS - added sections content and libraries - not styled

--- digital-solutions/js/main.js
+++ digital-solutions/js/main.js
@@ -1,7 +1,15 @@
+/**
+ * Template Name: Gp - v4.6.0
+ * Template URL: https://bootstrapmade.com/gp-free-multipurpose-html-bootstrap-template/
+ * Author: BootstrapMade.com
+ * License: https://bootstrapmade.com/license/
+ */
 ;(function () {
     "use strict"
 
-    // selector  function
+    /**
+     * Easy selector helper function
+     */
     const select = (el, all = false) => {
         el = el.trim()
         if (all) {
@@ -11,7 +19,9 @@
         }
     }
 
-    // listener function
+    /**
+     * Easy event listener function
+     */
     const on = (type, el, listener, all = false) => {
         let selectEl = select(el, all)
         if (selectEl) {
@@ -23,13 +33,19 @@
         }
     }
 
-    // on scroll  listener
-    const onscroll = (el, listener) => el.addEventListener("scroll", listener)
+    /**
+     * Easy on scroll event listener
+     */
+    const onscroll = (el, listener) => {
+        el.addEventListener("scroll", listener)
+    }
 
-    // remove hash from navbar
+    /**
+     * Navbar links active state on scroll
+     */
     let navbarlinks = select("#navbar .scrollto", true)
     const navbarlinksActive = () => {
-        let position = window.scrollY + 100
+        let position = window.scrollY + 200
         navbarlinks.forEach((navbarlink) => {
             if (!navbarlink.hash) return
             let section = select(navbarlink.hash)
@@ -47,10 +63,13 @@
     window.addEventListener("load", navbarlinksActive)
     onscroll(document, navbarlinksActive)
 
-    // Scroll to an element
+    /**
+     * Scrolls to an element with header offset
+     */
     const scrollto = (el) => {
         let header = select("#header")
         let offset = header.offsetHeight
+
         let elementPos = select(el).offsetTop
         window.scrollTo({
             top: elementPos - offset,
@@ -58,7 +77,9 @@
         })
     }
 
-    // Toggle .header-scrolled class to #header on scroll
+    /**
+     * Toggle .header-scrolled class to #header when page is scrolled
+     */
     let selectHeader = select("#header")
     if (selectHeader) {
         const headerScrolled = () => {
@@ -72,51 +93,72 @@
         onscroll(document, headerScrolled)
     }
 
-    // Back to top button
+    /**
+     * Back to top button
+     */
     let backtotop = select(".back-to-top")
     if (backtotop) {
         const toggleBacktotop = () => {
-            window.scrollY > 100
-                ? backtotop.classList.add("active")
-                : backtotop.classList.remove("active")
+            if (window.scrollY > 100) {
+                backtotop.classList.add("active")
+            } else {
+                backtotop.classList.remove("active")
+            }
         }
         window.addEventListener("load", toggleBacktotop)
         onscroll(document, toggleBacktotop)
     }
 
-    // Mobile nav
-    on("click", ".mobile-nav-toggle", (e) => {
-        const nav = document.querySelector(".mobile-nav-toggle")
+    /**
+     * Mobile nav toggle
+     */
+    on("click", ".mobile-nav-toggle", function (e) {
         select("#navbar").classList.toggle("navbar-mobile")
-        nav.classList.toggle("bi-list")
-        nav.classList.toggle("bi-x")
+        this.classList.toggle("bi-list")
+        this.classList.toggle("bi-x")
     })
 
-    // mobile nav dropdowns
-    on("click", ".navbar .dropdown > a",function (e) {
-        if (select("#navbar").classList.contains("navbar-mobile")) {
-            e.preventDefault()
-            this.nextElementSibling.classList.toggle("dropdown-active")
-        }
-    }, true)
-
-    // Scrool with ofset on links
-    on("click", ".scrollto",function (e) {
-        if (select(this.hash)) {
-            e.preventDefault()
-
-            let navbar = select("#navbar")
-            if (navbar.classList.contains("navbar-mobile")) {
-                navbar.classList.remove("navbar-mobile")
-                let navbarToggle = select(".mobile-nav-toggle")
-                navbarToggle.classList.toggle("bi-list")
-                navbarToggle.classList.toggle("bi-x")
+    /**
+     * Mobile nav dropdowns activate
+     */
+    on(
+        "click",
+        ".navbar .dropdown > a",
+        function (e) {
+            if (select("#navbar").classList.contains("navbar-mobile")) {
+                e.preventDefault()
+                this.nextElementSibling.classList.toggle("dropdown-active")
             }
-            scrollto(this.hash)
-        }
-    }, true)
+        },
+        true
+    )
 
-    // Scroll on page load with hash
+    /**
+     * Scrool with ofset on links with a class name .scrollto
+     */
+    on(
+        "click",
+        ".scrollto",
+        function (e) {
+            if (select(this.hash)) {
+                e.preventDefault()
+
+                let navbar = select("#navbar")
+                if (navbar.classList.contains("navbar-mobile")) {
+                    navbar.classList.remove("navbar-mobile")
+                    let navbarToggle = select(".mobile-nav-toggle")
+                    navbarToggle.classList.toggle("bi-list")
+                    navbarToggle.classList.toggle("bi-x")
+                }
+                scrollto(this.hash)
+            }
+        },
+        true
+    )
+
+    /**
+     * Scroll with ofset on page load with hash links in the url
+     */
     window.addEventListener("load", () => {
         if (window.location.hash) {
             if (select(window.location.hash)) {
@@ -125,7 +167,9 @@
         }
     })
 
-    // Preloader
+    /**
+     * Preloader
+     */
     let preloader = select("#preloader")
     if (preloader) {
         window.addEventListener("load", () => {
@@ -133,7 +177,103 @@
         })
     }
 
-    // Animation on scroll
+    /**
+     * Clients Slider
+     */
+    new Swiper(".clients-slider", {
+        speed: 1000,
+        loop: true,
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false,
+        },
+        slidesPerView: "auto",
+        pagination: {
+            el: ".swiper-pagination",
+            type: "bullets",
+            clickable: true,
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 2,
+                spaceBetween: 40,
+            },
+            480: {
+                slidesPerView: 3,
+                spaceBetween: 60,
+            },
+            640: {
+                slidesPerView: 4,
+                spaceBetween: 80,
+            },
+            992: {
+                slidesPerView: 2,
+                spaceBetween: 120,
+            },
+        },
+    })
+
+    /**
+     * Porfolio isotope and filter
+     */
+    window.addEventListener("load", () => {
+        let portfolioContainer = select(".portfolio-container")
+        if (portfolioContainer) {
+            let portfolioIsotope = new Isotope(portfolioContainer, {
+                itemSelector: ".portfolio-item",
+            })
+
+            let portfolioFilters = select("#portfolio-flters li", true)
+
+            on(
+                "click",
+                "#portfolio-flters li",
+                function (e) {
+                    e.preventDefault()
+                    portfolioFilters.forEach(function (el) {
+                        el.classList.remove("filter-active")
+                    })
+                    this.classList.add("filter-active")
+
+                    portfolioIsotope.arrange({
+                        filter: this.getAttribute("data-filter"),
+                    })
+                    portfolioIsotope.on("arrangeComplete", function () {
+                        AOS.refresh()
+                    })
+                },
+                true
+            )
+        }
+    })
+
+    /**
+     * Initiate portfolio lightbox
+     */
+    const portfolioLightbox = GLightbox({
+        selector: ".portfolio-lightbox",
+    })
+
+    /**
+     * Portfolio details slider
+     */
+    new Swiper(".portfolio-details-slider", {
+        speed: 400,
+        loop: true,
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            type: "bullets",
+            clickable: true,
+        },
+    })
+
+    /**
+     * Animation on scroll
+     */
     window.addEventListener("load", () => {
         AOS.init({
             duration: 1000,
